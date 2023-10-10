7ad82db95a227e833249f27f63fe9c5ecb28a6fd
lyamulyadi <lyamulyadi05@gmail.com>
2023-10-10 11:31:15 +0700

    js kembali semula

--- js/script.js
+++ js/script.js
@@ -62,9 +62,7 @@ function myFunction() {
   });
 });
 
-
-
-// product maintain CATEGORIE
+  // product maintain CATEGORIE
   document.addEventListener("DOMContentLoaded", function () {
     const productList = document.getElementById("product-list");
     const productModal = document.getElementById("product-modal");
@@ -72,98 +70,131 @@ function myFunction() {
     const closeModal = document.getElementsByClassName("close")[0];
     const cartList = document.getElementById("cart-items");
     const co = document.getElementById("coButton");
-
+  
     co.addEventListener('click', function () {
-        window.location.href = 'cart.html';
+      window.location.href = 'cart.html';
     });
+  
+    // Data produk masih manual
+    const products = [
+      { id: 1, name: "Produk 1", click: "Click for more", description : "Deskripsi produk 1", price: 100, img: "product/p12.jpeg" },
+      { id: 2, name: "Produk 2", click: "Click for more",description: "Deskripsi produk 2", price: 150, img: "product/p1.jpeg" },
+      { id: 3, name: "Produk 3", click: "Click for more",description: "Deskripsi produk 3", price: 200, img: "product/p2.jpeg" },
+      { id: 4, name: "Produk 4", click: "Click for more",description: "Deskripsi produk 3", price: 200, img: "product/p3.jpeg" },
+      { id: 5, name: "Produk 5", click: "Click for more",description: "Deskripsi produk 3", price: 200, img: "product/p4.jpeg" },
+      { id: 6, name: "Produk 6", click: "Click for more",description: "Deskripsi produk 3", price: 200, img: "product/p5.jpeg" },
+      { id: 7, name: "Produk 7", click: "Click for more",description: "Deskripsi produk 3", price: 200, img: "product/p6.jpeg" },
+    ];
 
-    // Fetch user data from the API
-    fetch("https://cute-frog-miniskirt.cyclic.app/users")
-        .then(response => response.json())
-        .then(users => {
-            // Loop through the user data and create product elements
-            users.forEach(user => {
-                const productItem = document.createElement("div");
-                productItem.classList.add("product");
+    
+  
+    // Tampilkan produk dalam halaman
+    products.forEach(product => {
+  
+      const productItem = document.createElement("div");
+      productItem.classList.add("product");
+  
+      productItem.innerHTML = `
+        <div class="product-image">
+          <img src="${product.img}" alt="${product.name}">
+        </div>
+        <div class="product-details">
+          <h3>${product.name}</h3>
+          <p>${product.click}</p>
+          <p>Harga: $${product.price}</p>
+        </div>
+        <div class="product-action">
+          <button class="add-to-cart">Add to Cart</button>
+        </div>
+      `;
+  
+      // Tambahkan event listener untuk menampilkan modal detail produk saat produk diklik
+      productItem.addEventListener("click", () => {
+        // Isi konten modal dengan informasi produk
+        productDetails.innerHTML = `
+          <h2>${product.name}</h2>
+          <p>Deskripsi: ${product.description}</p>
+          <p>Harga: $${product.price}</p>
+        `;
+  
+        // Tampilkan modal
+        productModal.style.display = "block";
+      });
+  
+      // Tambahkan produk ke daftar produk
+      productList.appendChild(productItem);
+  
 
-                productItem.innerHTML = `
-                    <div class="product-image">
-                        <img src="${user.img}" alt="${user.name}">
-                    </div>
-                    <div class="product-details">
-                        <h3>${user.name}</h3>
-                        <p>${user.click}</p>
-                        <p>Harga: $${user.price}</p>
-                    </div>
-                    <div class="product-action">
-                        <button class="add-to-cart">Add to Cart</button>
-                    </div>
-                `;
 
-                // Add event listener to show product details in the modal
-                productItem.addEventListener("click", () => {
-                    // Fill the modal content with user information
-                    productDetails.innerHTML = `
-                        <h2>${user.name}</h2>
-                        <p>Deskripsi: ${user.description}</p>
-                        <p>Harga: $${user.price}</p>
-                    `;
+      const addToCartButton = productItem.querySelector(".add-to-cart");
+      addToCartButton.addEventListener("click", function (event) {
+          event.stopPropagation(); // Prevent the productItem click event from firing
+          const itemName = product.name;
+          const itemPrice = product.price;
+      
+          // Buat elemen baru untuk item yang ditambahkan ke keranjang
+          const cartItem = document.createElement("li");
+          cartItem.textContent = itemName + " - " + itemPrice;
+      
+          // Buat elemen tombol hapus barang dari keranjang
+          const deleteButton = document.createElement("button");
+          deleteButton.textContent = "X";
+          deleteButton.className = "delete-button";
+          deleteButton.addEventListener("click", function () {
+              // Hapus elemen li saat tombol hapus ditekan
+              cartItem.remove();
+          });
+      
+          cartItem.appendChild(deleteButton);
+      
+          // Tambahkan item ke dalam keranjang
+          cartList.appendChild(cartItem);
+      
+          // Close the product modal when an item is added to the cart
+          productModal.style.display = "none";
+      
+          // Mengakses elemen dengan class "order-overflow" pada halaman cart.html
+          const orderOverflow = document.querySelector(".order-overflow");
+      
+          // Menambahkan cartItem ke dalam div dengan class "order-overflow"
+          orderOverflow.appendChild(cartItem);
+      });
+      
+  
 
-                    // Show the modal
-                    productModal.style.display = "block";
-                });
 
-                // Add product to the product list
-                productList.appendChild(productItem);
-
-                const addToCartButton = productItem.querySelector(".add-to-cart");
-                addToCartButton.addEventListener("click", function (event) {
-                    event.stopPropagation();
-                    const itemName = user.name;
-                    const itemPrice = user.price;
-
-                    // Create a new list item for the cart
-                    const cartItem = document.createElement("li");
-                    cartItem.textContent = itemName + " - " + itemPrice;
-
-                    // Create a delete button for the cart item
-                    const deleteButton = document.createElement("button");
-                    deleteButton.textContent = "X";
-                    deleteButton.className = "delete-button";
-                    deleteButton.addEventListener("click", function () {
-                        // Remove the list item when the delete button is clicked
-                        cartItem.remove();
-                    });
-
-                    cartItem.appendChild(deleteButton);
-
-                    // Add the item to the cart list
-                    cartList.appendChild(cartItem);
-                    productModal.style.display = "none";
-
-                    // Access the element with class "order-overflow" on the cart.html page
-                    const orderOverflow = document.querySelector(".order-overflow");
-
-                    // Add cartItem to the div with class "order-overflow"
-                    orderOverflow.appendChild(cartItem);
-                });
-            });
-        })
-        .catch(error => {
-            console.error("Error fetching data:", error);
-        });
-
-    // Add event listener to close the modal
+    // Tambahkan event listener untuk menutup modal
     closeModal.addEventListener("click", () => {
-        productModal.style.display = "none";
+      productModal.style.display = "none";
     });
-
-    // Close the modal when clicking outside of it
+  
+    // Tutup modal saat mengklik di luar area modal
     window.addEventListener("click", event => {
-        if (event.target === productModal) {
-            productModal.style.display = "none";
-        }
+      if (event.target === productModal) {
+        productModal.style.display = "none";
+      }
     });
+  
+    // Tambahkan event listener untuk menampilkan productDetails
+    const productDetailButtons = document.querySelectorAll(".product-details-button");
+    productDetailButtons.forEach(function (button) {
+      button.addEventListener("click", function () {
+        const product = button.parentElement.parentElement;
+        const productName = product.querySelector(".product-details h2").textContent;
+        const productDescription = product.querySelector(".product-details p").textContent;
+        const productPrice = product.querySelector(".product-details p:last-child").textContent;
+  
+        // Isi konten productDetails dengan informasi produk
+        productDetails.innerHTML = `
+          <h2>${productName}</h2>
+          <p>${productDescription}</p>
+          <p>${productPrice}</p>
+        `;
+  
+        // Tampilkan productDetails
+        productModal.style.display = "block";
+      });
+    });
+  });
 });
-
 
